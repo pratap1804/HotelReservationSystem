b2ad4204f40244a50330f83261af3e58958da96c
pratap1804 <pratap.1804@gmail.com>
2021-07-19 08:29:53 +0530

    Added: welcome message

---
+++ HotelReservationSystem/lib/src/main/java/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,11 @@
+
+package HotelReservationSystem;
+
+public class HotelReservation {
+	
+	//Displaying welcome message.
+	
+   public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation system!");
+    }
+}
